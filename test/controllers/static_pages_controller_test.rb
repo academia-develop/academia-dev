31a90da78cab61d2db6ecaefbf077e5b3418abf5
yuya <okumura.infratop@gmail.com>
2016-09-01 12:36:46 +0000

    filling-in-layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,10 +15,17 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   end
 
-   test "should get about" do
+  test "should get about" do
     get :about
     assert_response :success
     assert_select "title", "About|Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact|Ruby on Rails Tutorial Sample App"
+  end
+
 
 end
