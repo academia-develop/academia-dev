69ae02eb0b3a957003b6c31f9b8e53ca517a4d67
yuya <okumura.infratop@gmail.com>
2016-09-23 03:50:02 +0000

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,15 @@ module SessionsHelper
 
   # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -22,8 +30,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
