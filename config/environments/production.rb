c04193086f62a5d607b6c95805864acd7033db75
yuya <okumura.infratop@gmail.com>
2016-09-28 09:26:03 +0000

    text care

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = '【ここにHerokuのサブドメインを入力】.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings =1 {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
